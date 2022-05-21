934b3a923d94ba9835dae13e555a3f73c69fb2de
N3dal <nedalxzo@gmail.com>
2022-05-22 00:19:47 +0200

    set the basic configuration to our window.

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def clear():
         system("cls")
 
 
-clear(1)
+clear()
 
 
 def start_app(root: tkinter.Tk):
@@ -74,6 +74,18 @@ def main_window():
     """main window for the calculator."""
 
     root = tkinter.Tk()
+
+    root.title(WIN_TITLE)
+
+    # set the background.
+    root.configure(bg=WIN_BG)
+
+    # make the window un-resizable.
+    root.resizable(False, False)
+
+    # set the size of our window and also the start position.
+    root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
+
 
     start_app(root)
 
